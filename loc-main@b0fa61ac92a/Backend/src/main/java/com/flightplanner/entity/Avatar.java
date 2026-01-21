87f065692567e65708a10cd499c82e69bfeb7701
Fredrik <37407836+fredrikdn@users.noreply.github.com>
2026-01-21 10:50:21 +0100

    Delete loc-main@b0fa61ac92a directory

--- loc-main@b0fa61ac92a/Backend/src/main/java/com/flightplanner/entity/Avatar.java
+++
@@ -1,5 +0,0 @@
-package com.flightplanner.entity;
-
-// TODO: implementere avatar entity (må ha laget en tabell i databasen først)
-public class Avatar {
-}
--- loc-main@b0fa61ac92a/frontend/src/App.tsx
+++
@@ -1,2 +0,0 @@
-// Re-export consolidated App from modules/app for single source.
-export { App } from './modules/app/App';
